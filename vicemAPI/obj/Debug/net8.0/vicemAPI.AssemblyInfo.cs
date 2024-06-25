ee9017fc6742970c9cd941fa44b27b0aad69dcbf
08Trang <trang.hello@gmail.com>
2024-06-25 17:11:56 +0700

    Add_FullName

--- vicemAPI/obj/Debug/net8.0/vicemAPI.AssemblyInfo.cs
+++ vicemAPI/obj/Debug/net8.0/vicemAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vicemAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b69e4d7b005a685e1dbc4da5c4507efd1d7804")]
 [assembly: System.Reflection.AssemblyProductAttribute("vicemAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vicemAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
